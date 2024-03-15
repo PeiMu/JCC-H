507202e437d887c285a06118b58cbdb3cca08555
Pei Mo <pei@luna.inf.ed.ac.uk>
2024-03-15 16:46:36 +0000

    add username and dbname

--- execute_queries.sh
+++ execute_queries.sh
@@ -7,8 +7,8 @@ mkdir -p result/
 for i in $(eval echo {1.."${iteration}"}); do
   for sql in "${dir}"/*; do
     echo "execute ${sql}" 2>&1|tee -a skinner_explained_jcch-1_${i}.txt;
-    psql -f "${sql}" 2>&1|tee -a skinner_explained_jcch-1_${i}.txt;
-    #psql -f "${sql}";
+    psql -U jcc -d jcc -f "${sql}" 2>&1|tee -a skinner_explained_jcch-1_${i}.txt;
+    #psql -U jcc -d jcc -f "${sql}";
   done
 done
 
